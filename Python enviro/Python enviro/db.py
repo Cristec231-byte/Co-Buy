245ef354580df3304dc738b4a2ae55e7b67a27b5
fazecheese <faizanahmed.rep@gmail.com>
2025-06-12 13:27:26 +1200

    Remove old CRUD
    Old folder

--- Python enviro/Python enviro/db.py
+++
@@ -1,8 +0,0 @@
-# db.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://postgres:cobuy@localhost:5432/Cobuy"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
